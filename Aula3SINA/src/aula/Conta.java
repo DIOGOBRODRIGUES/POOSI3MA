73ec221af6de1e3fbb40b2dbf28e1de923221bad
Diogo Rodrigues <diogo.borbar@gmail.com>
2021-02-25 20:54:28 -0300

    final do exemplo

--- Aula3SINA/src/aula/Conta.java
+++ Aula3SINA/src/aula/Conta.java
@@ -4,6 +4,16 @@ public class Conta {
 	private String numero;
 	private double saldo;
 	
+	public Conta() {
+		numero = "";
+		saldo = 0.0;
+	}
+	
+	public Conta (String numeroInicial, double saldoInicial) {
+		numero = numeroInicial;
+		saldo = saldoInicial;
+	}
+	
 	public void creditar (double valor) {
 		saldo = saldo + valor;
 	}
