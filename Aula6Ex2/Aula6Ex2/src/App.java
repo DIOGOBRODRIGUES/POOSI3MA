9e19dc818fe0316f0b566d8e5a8f15739246041f
Diogo Rodrigues <diogo.borbar@gmail.com>
2021-04-01 09:27:38 -0300

    Aula6Ex2 part 1

--- Aula6Ex2/Aula6Ex2/src/App.java
+++ Aula6Ex2/Aula6Ex2/src/App.java
@@ -6,5 +6,36 @@ public class App {
            }
            System.out.println("");
        }
+    //padrão 1
+       for(int i = 0; i < 10; i++){
+           for (int j = 0; j < 10; j++) {
+            if(i>=j){
+             System.out.print("*"); 
+            }
+           }
+           System.out.println("");
+       }
+       System.out.println("");
+       //padrão 2
+       for(int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++) {
+         if(i+j<10){
+          System.out.print("*"); 
+         }
+        }
+        System.out.println("");
+    }
+       //padrão 3
+       System.out.println("");
+       for(int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++) {
+         if(j>=i){
+          System.out.print("*"); 
+         }else{
+             System.out.print(" ");
+         }
+        }
+        System.out.println("");
+    }
     }
 }
