5c79f73f016515b56672f62d6771fe9537440427
Diogo Rodrigues <diogo.borbar@gmail.com>
2021-04-01 09:00:02 -0300

    Primeiro Commit

---
+++ Aula6Ex2/Aula6Ex2/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       for (int linha = 0; linha < 10; linha++){
+           for(int coluna = 0; coluna < 10; coluna++){
+           System.out.print(linha+"*"+coluna+" ");
+           }
+           System.out.println("");
+       }
+    }
+}
