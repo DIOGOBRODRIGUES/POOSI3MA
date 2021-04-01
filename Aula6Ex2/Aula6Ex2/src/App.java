af29f48c3cf3d3e2d25bd158b685234eaf73f268
Diogo Rodrigues <diogo.borbar@gmail.com>
2021-04-01 09:34:53 -0300

    Aula6Ex2 resolvido

--- Aula6Ex2/Aula6Ex2/src/App.java
+++ Aula6Ex2/Aula6Ex2/src/App.java
@@ -37,5 +37,18 @@ public class App {
         }
         System.out.println("");
     }
+
+    //padrão 4 
+      System.out.println("");
+      for(int i = 0; i < 10; i++){
+       for (int j = 0; j < 10; j++) {
+        if(i+j>=9){
+         System.out.print("*"); 
+        }else{
+            System.out.print(" ");
+        }
+       }
+       System.out.println("");
+   }
     }
 }
