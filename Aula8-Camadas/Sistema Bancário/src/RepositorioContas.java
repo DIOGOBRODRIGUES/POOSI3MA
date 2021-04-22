221c7a8bc4f4d0852919b09df06727062c89affb
Diogo Rodrigues <diogo.borbar@gmail.com>
2021-04-22 10:09:02 -0300

    remover Conta

--- Aula8-Camadas/Sistema Bancário/src/RepositorioContas.java
+++ Aula8-Camadas/Sistema Bancário/src/RepositorioContas.java
@@ -6,18 +6,19 @@ public class RepositorioContas {
         this.contas = new Conta[tamanho];
         this.proxima = 0;
     }
-
+//***************Cadastrar Conta****************** */
     public void cadastrar (Conta c){
         this.contas[this.proxima] = c;
         this.proxima = this.proxima + 1;
     }
 
+//*******************Procurar Conta*************** */
     public Conta procurar (String num){
         int i = 0;
         boolean achou = false;
 
         while((!achou) && (i < this.proxima)){
-            if(num.equals(this.contas[i].getNumero())){
+            if(num.equals(String.valueOf(this.contas[i].getNumero()))){
                 achou = true;
             }else{
                 i = i + 1;
@@ -30,5 +31,27 @@ public class RepositorioContas {
         }
         return resultado;
 
+    }
+//***********************Remover Conta************* */
+
+    public void remover(String num){
+        int i  = 0;
+        boolean achou = false;
+        while((!achou) && (i < this.proxima)){
+            if(num.equals(String.valueOf(this.contas[i].getNumero()))){
+                achou = true;
+            }else{
+                i = i+ 1;
+            }
+        }
+
+        if (i != this.proxima){
+            this.contas[i] = this.contas[this.proxima - 1];
+            this.contas[this.proxima - 1] = null;
+            this.proxima = this.proxima - 1;
+            System.out.println("Conta" + num + "foi removida");
+        }else{
+            System.out.println("Conta não existe");
+        }
     }
 }
